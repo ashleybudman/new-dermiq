9e98609c587c8f75af1ed65e44bf7c4c1d524603
Ashley Budman <ashleybudman@umail.ucsb.edu>
2024-02-22 16:35:15 -0800

    add to app and index

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "newdermiq22582ae2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
